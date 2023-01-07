c417bfc65e71c70b7453688f1e3fa0f614191cf7
Antoine Moenaert <amo.ext@megabyte.be>
2023-01-07 17:56:36 +0100

    Handle PEPassport == 0

--- WeldingPassportsApp/Infrastructure/Repositories/API/CertificatesAPIRepository.cs
+++ WeldingPassportsApp/Infrastructure/Repositories/API/CertificatesAPIRepository.cs
@@ -31,92 +31,106 @@ namespace Infrastructure.Repositories.API
         {
             AppSettings app = await _appSettingsSQLRepository.GetAppsetingsAsync();
 
-            var allowedRegistrationTypesQ = _context.PEPassports
-                .Where(pePassport => pePassport.ID == pePassportID)
-                .Include(pePassport => pePassport.Registrations)
-                .FirstOrDefault()
-                .Registrations;
-
-            IEnumerable<CurrentRegistration> allowedRegistrationTypesQQ = null;
-
-            if (allowedRegistrationTypesQ.Count() != 0)
-            {
-                allowedRegistrationTypesQQ = allowedRegistrationTypesQ.Join(
-                        _context.Examinations.DefaultIfEmpty(),
-                        registration => new
-                        {
-                            ExaminationID = registration.ExaminationID
-                        },
-                        examination => new
-                        {
-                            ExaminationID = examination.ID
-                        },
-                        (registration, examination) => new CurrentRegistration
-                        {
-                            RegistrationTypeID = registration.RegistrationTypeID,
-                            ProcessID = registration.ProcessID,
-                            CompanyID = registration.CompanyID,
-                            ExpiryDate= registration.ExpiryDate,
-                            Examination = examination,
-                            HasPassed = registration.HasPassed,
-                            Revoke = registration.Revoke
-                        })
-                    .OrderByDescending(registration => registration.Examination.ExamDate)
-                    .Take(1);
-            }
-            else
-            {
-                allowedRegistrationTypesQQ = new Collection<CurrentRegistration>()
-                    {
-                        new CurrentRegistration()
-                        {
-                            RegistrationTypeID = null,
-                            ProcessID = null,
-                            CompanyID = null,
-                            ExpiryDate = null,
-                            Examination = null,
-                            HasPassed = null,
-                            Revoke = null
-                        }
-                    };
-            }
-
-            var allowedRegistrationTypesQQQ = allowedRegistrationTypesQQ.Select(registration => new
+            IEnumerable<RegistrationType> allowedRegistrationTypes = _context.RegistrationTypes
+                .Where(registrationType => registrationType.IsActive)
+                .Select(registrationType => new RegistrationType
                 {
-                    RegistrationTypeID = (int?) registration.RegistrationTypeID,
-                    ExtendableStatus =
-                        registration.Revoke != null ? ExtendableStatus.Revoked :
-                        EF.Functions.DateDiffDay(DateTime.Now, registration.ExpiryDate) > app.MaxInAdvanceDays ? ExtendableStatus.NotYetExtendable :
-                        (EF.Functions.DateDiffDay(DateTime.Now, registration.ExpiryDate) > (app.MaxExtensionDays * -1) ? ExtendableStatus.Extendable :
-                        ExtendableStatus.NoMoreExtendable),
-                    HasPassed = registration.HasPassed
+                    ID = registrationType.ID,
+                    RegistrationTypeName = registrationType.RegistrationTypeName
                 });
 
-            var allowedRegistrationTypes = allowedRegistrationTypesQQQ.Join(
-                    _context.AllowedRegistrationTypes.Include(allowedRegistrationTypes => allowedRegistrationTypes.AvailableRegistrationType).DefaultIfEmpty(),
-                    registration => new
+            IEnumerable<CurrentRegistration> allowedRegistrationTypesQQQ = new Collection<CurrentRegistration>()
+                {
+                    new CurrentRegistration()
                     {
-                        RegistrationTypeID = registration.RegistrationTypeID,
-                        ExtendableStatus = registration.ExtendableStatus,
-                        HasPassed = registration.HasPassed
-                    },
-                    allowedRegistrationTypes => new
-                    {
-                        RegistrationTypeID = allowedRegistrationTypes.RegistrationTypeID,
-                        ExtendableStatus = allowedRegistrationTypes.ExtendableStatus,
-                        HasPassed = allowedRegistrationTypes.HasPassed
-                    },
-                    (registration, allowedRegistrationTypes) => new RegistrationType
-                    {
-                        ID = allowedRegistrationTypes.AvailableRegistrationType.ID,
-                        RegistrationTypeName = allowedRegistrationTypes.AvailableRegistrationType.RegistrationTypeName
+                        RegistrationTypeID = null,
+                        ProcessID = null,
+                        CompanyID = null,
+                        ExpiryDate = null,
+                        Examination = null,
+                        HasPassed = null,
+                        Revoke = null
                     }
-                );
+                };
+
+            if(pePassportID != 0)
+            {
+                var allowedRegistrationTypesQ = _context.PEPassports
+                    .Where(pePassport => pePassport.ID == pePassportID)
+                    .Include(pePassport => pePassport.Registrations)
+                    .FirstOrDefault();
+
+                if (allowedRegistrationTypesQ == null)
+                {
+                    return null;
+                }
+
+                var allowedRegistrationTypesQQ = allowedRegistrationTypesQ
+                    .Registrations;
+
+                if (allowedRegistrationTypesQQ.Count() != 0)
+                {
+                    allowedRegistrationTypesQQQ = allowedRegistrationTypesQQ.Join(
+                            _context.Examinations.DefaultIfEmpty(),
+                            registration => new
+                            {
+                                ExaminationID = registration.ExaminationID
+                            },
+                            examination => new
+                            {
+                                ExaminationID = examination.ID
+                            },
+                            (registration, examination) => new CurrentRegistration
+                            {
+                                RegistrationTypeID = registration.RegistrationTypeID,
+                                ProcessID = registration.ProcessID,
+                                CompanyID = registration.CompanyID,
+                                ExpiryDate= registration.ExpiryDate,
+                                Examination = examination,
+                                HasPassed = registration.HasPassed,
+                                Revoke = registration.Revoke
+                            })
+                        .OrderByDescending(registration => registration.Examination.ExamDate)
+                        .Take(1);
+                }
+
+                var allowedRegistrationTypesQQQQ = allowedRegistrationTypesQQQ.Select(registration => new
+                    {
+                        RegistrationTypeID = (int?) registration.RegistrationTypeID,
+                        ExtendableStatus =
+                            registration.Revoke != null ? ExtendableStatus.Revoked :
+                            EF.Functions.DateDiffDay(DateTime.Now, registration.ExpiryDate) > app.MaxInAdvanceDays ? ExtendableStatus.NotYetExtendable :
+                            (EF.Functions.DateDiffDay(DateTime.Now, registration.ExpiryDate) > (app.MaxExtensionDays * -1) ? ExtendableStatus.Extendable :
+                            ExtendableStatus.NoMoreExtendable),
+                        HasPassed = registration.HasPassed
+                    });
+
+                allowedRegistrationTypes = allowedRegistrationTypesQQQQ.Join(
+                        _context.AllowedRegistrationTypes.Include(allowedRegistrationTypes => allowedRegistrationTypes.AvailableRegistrationType).DefaultIfEmpty(),
+                        registration => new
+                        {
+                            RegistrationTypeID = registration.RegistrationTypeID,
+                            ExtendableStatus = registration.ExtendableStatus,
+                            HasPassed = registration.HasPassed
+                        },
+                        allowedRegistrationTypes => new
+                        {
+                            RegistrationTypeID = allowedRegistrationTypes.RegistrationTypeID,
+                            ExtendableStatus = allowedRegistrationTypes.ExtendableStatus,
+                            HasPassed = allowedRegistrationTypes.HasPassed
+                        },
+                        (registration, allowedRegistrationTypes) => new RegistrationType
+                        {
+                            ID = allowedRegistrationTypes.AvailableRegistrationType.ID,
+                            RegistrationTypeName = allowedRegistrationTypes.AvailableRegistrationType.RegistrationTypeName
+                        }
+                    );
+            }
 
             return new GetGetRegistrationTypesFromPEPassportReponse
                     {
-                        CompanyID = allowedRegistrationTypesQQ.FirstOrDefault()?.CompanyID,
-                        ProcessID = allowedRegistrationTypesQQ.FirstOrDefault()?.ProcessID,
+                        CompanyID = allowedRegistrationTypesQQQ.FirstOrDefault()?.CompanyID,
+                        ProcessID = allowedRegistrationTypesQQQ.FirstOrDefault()?.ProcessID,
                         RegistrationsSelectList = new SelectList(allowedRegistrationTypes, nameof(RegistrationType.ID), "RegistrationTypeName")
                     };
         }
